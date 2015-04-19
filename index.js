535a4a61aa82c82fe09aafa9ec9c9fce50826eba
Manuel Astudillo <manuel@optimalbits.com>
2015-04-19 15:06:37 +0200

    added auto retry functionality for events

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var request = require('request');
 var EventEmitter = require('events').EventEmitter;
 var Promise = require('bluebird');
 
+var RESTART_WAIT = 5000;
 
 var Dolphin = function(_url, opts) {
   if (!(this instanceof Dolphin)) return new Dolphin(_url, opts);
@@ -14,7 +15,36 @@ var Dolphin = function(_url, opts) {
 
   this.url = url.parse(_url);
   this.opts = opts;
+
+  var _this = this;
+
+  this.containers.inspect = function(id){
+  	return _this._get('containers/' + id + '/json');
+  }
+
+  this.containers.logs = function(id){
+  	// TODO: disable json parse
+  	return _this._get('containers/' + id + '/logs');
+  }
+
+  this.containers.changes = function(id){
+  	return _this._get('containers/' + id + '/changes');
+  }
+
+  this.containers.export = function(id){
+  	// TODO: disable json parse
+  	return _this._get('containers/' + id + '/export');
+  }
+
+  this.containers.stats = function(id){
+  	// TODO: "getStream"
+  	return _this._get('containers/' + id + '/stats');
+  }
 };
+
+Dolphin.prototype.containers = function(query){
+	return this._get('containers/json', query);
+}
 
 Dolphin.prototype.info = function() {
 	return this._get('info');
@@ -25,40 +55,80 @@ Dolphin.prototype.version = function() {
 }
 
 Dolphin.prototype.events = function(opts) {
+	opts = opts || {};
+
+	var url = this.url.href + '/events';
 	var emitter = new EventEmitter;
+	var latestTime;
+	var req;
+	var restartTimeout;
 
-	var req = request({
-		url: this.url.href + '/events',
-		qs: opts
-	}).on('data', function(chunk){
-		var evt;
-		try{
-			evt = JSON.parse(chunk.toString());
-			emitter.emit('event', evt);
-		}catch(err){
+	emitter.abort = function(){
+		req && req.abort();
+		clearTimeout(restartTimeout);
+	}
+
+	function startStreaming(){
+		return request({
+			url: url,
+			qs: opts
+		}).on('data', function(chunk){
+			var evt;
+			try{
+				evt = JSON.parse(chunk.toString());
+				latestTime = evt.time;
+				emitter.emit('event', evt);
+			}catch(err){
+				emitter.emit('error', err);
+			}
+		}).on('error', function(err){
 			emitter.emit('error', err);
-		}
-	}).on('error', function(err){
-		emitter.emit('error', err);
-	});
+			restart();
+		}).on('end', function(){
+			restart();
+		}).on('close', function(doc){
+			restart();
+		})
+	}
 
-	emitter.req = req;
+	function restart(){
+		setTimeout(function(){
+			if(latestTime){
+				opts.since = latestTime;
+			}
+			req = startStreaming();
+		}, RESTART_WAIT);
+	}
+
+	req = startStreaming(emitter);
+
 	return emitter;
 }
 
-Dolphin.prototype._get = function(path){
+Dolphin.prototype._get = function(path, query){
 	var href = this.url.href + '/' + path;
-	console.log(href)
+
+	var opts = {
+		url: href
+	};
+
+	if(query){
+		opts.qs = query;
+	}
+
 	return new Promise(function(resolve, reject){
-		request(href, function(err, response, body){
-			console.log(body)
+		request(opts, function(err, response, body){
 			if(err) return reject(err);
 
-			 try{
-			 	resolve(JSON.parse(body));
-			 }catch(err){
+			if(response.statusCode != 200){
+				return reject(Error('Request failed: '+response.statusCode));
+			}
+
+			try{
+				resolve(JSON.parse(body));
+			}catch(err){
 			 	reject(err);
-			 }
+			}
 		});
 	});
 }
